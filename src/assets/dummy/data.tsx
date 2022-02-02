a4dc0b3aa6a4e29c6c172dbb4c8d13d4d4732470
Fernando Aponte <fernandoaponte90@gmail.com>
2022-02-02 04:56:24 -0400

    data dummy

---
+++ src/assets/dummy/data.tsx
@@ -0,0 +1,64 @@
+const Data = [
+    {
+        alt: "Agila",
+        category: "aves",
+        srcImg: "https://www.nationalgeographic.com.es/medio/2018/02/27/aguila__1280x720.jpg",
+        srcVideo: "https://youtu.be/oUUtpZLPg_Q"
+    },
+    {
+        alt: "Colibrie",
+        category: "aves",
+        srcImg: "https://dam.ngenespanol.com/wp-content/uploads/2020/12/colibries-2.jpg",
+        srcVideo: null
+    },
+    {
+        alt: "Perro",
+        category: "mamiferos",
+        srcImg: "https://www.caracteristicas.co/wp-content/uploads/2017/02/perro-3-e1561679226953.jpg",
+        srcVideo: "https://youtu.be/EhJcdn7rCwc"
+    },
+    {
+        alt: "Elefante",
+        category: "mamiferos",
+        srcImg: "https://www.ejemplos.co/wp-content/uploads/2015/05/elefante-herbivoro-e1475100795272.jpg",
+        srcVideo: null
+    },
+    {
+        alt: "Rana Punta Flecha Azul",
+        category: "anfibios",
+        srcImg: "https://www.diainternacionalde.com/imagenes/dias/abril/04-24_dia-conservacion-anfibios_azul_m.jpg",
+        srcVideo: null
+    },
+    {
+        alt: "Camaleon",
+        category: "reptiles",
+        srcImg: "https://plustatic.com/3979/conversions/tipos-reptiles-default.jpg",
+        srcVideo: null
+    },
+    {
+        alt: "Iguana",
+        category: "reptiles",
+        srcImg: "https://t1.uc.ltmcdn.com/images/9/9/1/img_como_cuidar_una_iguana_42199_orig.jpg",
+        srcVideo: null
+    },
+    {
+        alt: "Pez Cirujano Azyl",
+        category: "peces",
+        srcImg: "https://static.nationalgeographicla.com/files/styles/image_3200/public/blue-tang-nationalgeographic_2175267.jpg?w=1600&h=1067",
+        srcVideo: null
+    },
+    {
+        alt: "Pez Payaso",
+        category: "peces",
+        srcImg: "https://www.anipedia.net/imagenes/pez-payaso.jpg",
+        srcVideo: "https://youtu.be/6gMZm1yRak0"
+    },
+    {
+        alt: "Pez Dorado",
+        category: "peces",
+        srcImg: "https://www.ngenespanol.com/wp-content/uploads/2018/09/peces-crecen-2.png",
+        srcVideo: null
+    },
+];
+
+export default Data;
